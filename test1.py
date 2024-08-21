6760977b209cd13378400ef261f3dba3494e5f5b
Abdurrahman Stjärnborg <apo.b@hotmail.se>
2024-08-21 13:52:25 +0200

    semestertilllägg

--- test1.py
+++ test1.py
@@ -1,1 +1,5 @@
 hej
+
+Den anställde har 25 000 kronor i månadslön.
+Varje semesterdag innebär 107:50 kronor i semestertillägg (0,0043 X 25 000 kronor)
+Om den anställde tar ut 20 semesterdagar blir semestertillägget 2 150 kronor (0,0043 X 20 X 25 000)28 maj 2024
